a69fed49cc1e8d5d895c30d9321ecd238e40ce26
Tomasz Fidos <fidostomasz@gmail.com>
2023-06-08 12:44:34 +0200

    Added login and signup elements to AppNav.

--- src/components/AppNav.tsx
+++ src/components/AppNav.tsx
@@ -10,13 +10,22 @@ const AppNav = (): JSX.Element => {
     const totalQuantity = useSelector(selectTotalQuantity);
     
     return (
-        <nav className="nav">
-            <ul>
-                <li><Link to="/" className="navElement">Strona Główna</Link></li>
-                <li><Link to="/sklep" className="navElement">Sklep</Link></li>
-                <li><Link to="/koszyk" className="navElement">Koszyk ({totalQuantity})</Link></li>
-            </ul>
-        </nav>
+        <div className="nav-container">
+            <nav className="nav-side"></nav>
+            <nav className="nav nav-main">
+                <ul>
+                    <li><Link to="/" className="nav-element">Strona Główna</Link></li>
+                    <li><Link to="/sklep" className="nav-element">Sklep</Link></li>
+                    <li><Link to="/koszyk" className="nav-element">Koszyk ({totalQuantity})</Link></li>
+                </ul>
+            </nav>
+            <nav className="nav nav-side">
+                <ul>
+                    <li><Link to="/login" className="nav-element">Login</Link></li>
+                    <li><Link to="/signup" className="nav-element">Rejestracja</Link></li>
+                </ul>
+            </nav>
+        </div>
     );
 }
 

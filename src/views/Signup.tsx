c8675882c92ea3e8b508112762d9182e160b9394
Tomasz Fidos <fidostomasz@gmail.com>
2023-06-18 16:53:11 +0200

    Added signup form html.

--- src/views/Signup.tsx
+++ src/views/Signup.tsx
@@ -1,6 +1,16 @@
+import './Views.css';
+
 const Signup = (): JSX.Element => {
     return (
-        <div>Signup</div>
+        <form className="form">
+            <input className="input" placeholder="Nazwa użytkownika" />
+            <input className="input" placeholder="Email" />
+            <input className="input" placeholder="Hasło" />
+            <input className="input" placeholder="Powtórz hasło" />
+            <div>
+                <button className="button" type="submit">Zarejestruj się</button>
+            </div>
+        </form>
     );
 }
 
